7dbe1351ddff665512fe7a560a708fe7210e313a
Ambady Rajeevan <ambadyrajeevan937@gmail.com>
2026-09-14 20:51:15 +0530

    Fix Django app loading before WebSocket routing

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -7,12 +7,10 @@ os.environ.setdefault(
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from chat.routing import websocket_urlpatterns
-
-
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
