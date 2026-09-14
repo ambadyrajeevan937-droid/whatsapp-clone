a0e9d38de132da8e87b8fc290156550504cbbfe6
Ambady Rajeevan <ambadyrajeevan937@gmail.com>
2026-09-14 20:43:13 +0530

    Fix ASGI settings configuration

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,15 +1,16 @@
 import os
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from chat.routing import websocket_urlpatterns
-
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings"
 )
+
+from django.core.asgi import get_asgi_application
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.routing import websocket_urlpatterns
+
 
 django_asgi_app = get_asgi_application()
 
